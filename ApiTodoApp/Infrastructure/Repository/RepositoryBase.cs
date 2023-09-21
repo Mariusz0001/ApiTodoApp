447470c09f1ade59383a7dfc62f6ed9b43c1f69c
Mariusz Pawlus <mariusz.pawlus95@gmail.com>
2023-09-21 10:16:15 +0200

    added RepositoryBase

---
+++ ApiTodoApp/Infrastructure/Repository/RepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiTodoApp.Infrastructure.Repository
+{
+    public class RepositoryBase<TDbContext> where TDbContext:DbContext
+    {
+        protected TDbContext DbContext { get; }
+
+        protected RepositoryBase(TDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+    }
+}
---
+++ ApiTodoApp/Repositories/PersonalTasksRepository.cs
@@ -0,0 +1,13 @@
+﻿using ApiTodoApp.Infrastructure.Database;
+using ApiTodoApp.Infrastructure.Repository;
+
+namespace ApiTodoApp.Repositories
+{
+    public class PersonalTasksRepository : RepositoryBase<ApplicationDbContext>
+    {
+        public PersonalTasksRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
